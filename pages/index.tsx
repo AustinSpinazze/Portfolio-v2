3819129c22c01729df08702b0c9a5d3375f4c879
AustinSpinazze <aspinazze94@gmail.com>
2022-11-23 20:52:12 -0600

    Initial header complete - Added Header and Layout components - Added Framer Motion - Added Social Icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
+
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
-			<Head>
-				<title>{"Austin's Portfolio"}</title>
-				<meta
-					name='description'
-					content='Developer portfolio for Austin Spinazze'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-
 			{/* Header */}
 
 			{/* Hero */}
